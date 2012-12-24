85b7b0274f124ebdc5ed674c87b8a88df7e09caf
Luke Scalf <luke@monkeywrench.cc>
2012-12-24 15:40:15 -0700

    Adding Facade.

---
+++ src/GetNinja/Gravatar/Facades/Gravatar.php
@@ -0,0 +1,12 @@
+<?php namespace GetNinja\Gravatar\Facades;
+
+class Gravatar extends Facade {
+
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor() { return 'gravatar'; }
+
+}
